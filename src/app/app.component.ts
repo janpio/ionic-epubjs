d7866193792868baa7eabdf5abff565851693cb3
Jan Piotrowski <piotrowski@gmail.com>
2017-07-19 19:41:45 +0200

    remove unnecessary import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { BookPage } from './../pages/book/book';
 
 @Component({
   templateUrl: 'app.html'
